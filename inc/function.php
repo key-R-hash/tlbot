f22a57a0998db9766407ccb48b0948016d1492fe
kiarash.amiri <kiarash.amiri80@gmail.com>
2018-11-22 17:42:20 +0330

    final 1

--- inc/function.php
+++ inc/function.php
@@ -1,6 +1,23 @@
 <?php
+
+    // function getfirst(){
+    //     $data = file_get_contents("https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/getUpdates");
+    //     $data = json_decode($data, true);
+    //     $count = count($data['result']) - 1 ;
+    //     $data = $data['result'][$count]['message']['text'];
+    //     return $data;
+    // }
+    
+    // function getsecond(){
+    //     $data = file_get_contents("https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/getUpdates");
+    //     $data = json_decode($data, true);
+    //     $count = count($data['result']) - 2 ;
+    //     $data = $data['result'][$count]['message']['text'];
+    //     return $data;
+    // }
  function query_login(){
     global $chatId;
+
     $connection = new mysqli('localhost','kiarash','Skills39*','wp');
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
@@ -16,6 +33,7 @@
  }
  function login(){
     global $chatId;
+
     $connection = new mysqli('localhost','kiarash','Skills39*','wp');
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
@@ -30,6 +48,7 @@
  }
  function query_massage(){
     global $chatId;
+
     $connection = new mysqli('localhost','kiarash','Skills39*','wp');
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
@@ -44,6 +63,7 @@
 }
 function query_massage_id(){
     global $chatId;
+
     $connection = new mysqli('localhost','kiarash','Skills39*','wp');
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
@@ -57,6 +77,7 @@ function query_massage_id(){
     return $ms;
 }
 function query_function(){
+
     $connection = new mysqli('localhost','kiarash','Skills39*','wp');
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
@@ -68,31 +89,43 @@ function query_function(){
 
         $fu[] = $fun['-command-'];
     }
+
     return $fu;
 }
 function query($massage){
+
+    global $update_id;
 $now = time();
-global $chatId;
+
+    global $chatId;
+
+
 $connection = new mysqli('localhost','kiarash','Skills39*','wp');
 if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
 }
 $query_str = "SELECT  `chat_id` FROM `login` where chat_id = $chatId and status = 1";
+
 $query = $connection->query($query_str);
 $as = $query->fetch_assoc();
 $as = $as['chat_id'];
+
 if($massage == '-username-'){
     global $update_id;
     $query_str = "SELECT  `username` FROM `login` where chat_id = $chatId and `status` = 0 and `functions` = '-username-'";
     $query = $connection->query($query_str);
     $posts = $query->fetch_assoc();
+
     if($posts == null){
-    $query_str = "INSERT INTO `login`(`functions`,`chat_id`, `username`,`status`,`password`,`date`) VALUES ('-username-',$chatId,'0','0','0',$now)";
+
+        $query_str = "INSERT INTO `login`(`functions`,`chat_id`, `username`,`status`,`password`,`date`) VALUES ('-username-',$chatId,'0','0','0',$now)";
 	$query = $connection->query($query_str);
         $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=username:";
         file_get_contents($url); 
         die();
+
     }
+
 }
 if($massage == '-password-'){
     $query_str = "SELECT  `username` FROM `login` where chat_id = $chatId and `status` = 0 and `functions` = '-password-'";
@@ -101,20 +134,27 @@ if($massage == '-password-'){
     if($posts == null){
         $query_str = "UPDATE `login` SET `functions` = '-password-'  where `chat_id` = $chatId ";
         $query = $connection->query($query_str);
+
         $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=password: ";
         file_get_contents($url);
         die();
+
     }
+
 }
 if($massage == '-finish_login-'){
     $query_str = "SELECT  `password`,`username` FROM `login` where chat_id = $chatId and status = 1";
     $query = $connection->query($query_str);
     $posts = $query->fetch_assoc();
+
     $query_str = "SELECT  `password`,`username` FROM `login` where chat_id = $chatId and `status` = 0 ";
     $query = $connection->query($query_str);
     $user = $query->fetch_assoc();
+
     $login = wp_login($user['username'],$user['password'],'wp_signon()');
+
     if($posts == null && $login == 1){
+        
         $username = "'" . $data . "'";
         $query_str = "UPDATE `login` SET `status`= 1 where chat_id = $chatId ";
         $query = $connection->query($query_str);
@@ -124,26 +164,30 @@ if($massage == '-finish_login-'){
         file_get_contents($url);
         die();
     }elseif($login == 0 || $login == null){
+
         $query_str = "DELETE FROM `login` WHERE `chat_id` = $chatId ";
         $query = $connection->query($query_str); 
-        $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=your username or password incorrect inter your username and password again";
+        $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=your username or password incorrect please inter your username and password again";
         file_get_contents($url);
         die();
     }
 }
+
 if($massage == '-title-' || $massage == '-body-' || $massage == '-finish-' || $massage == '-start-' && $as != null){
     if($massage == '-start-'){
     $query_str = "SELECT  `password`,`username` FROM `login` where chat_id = $chatId and `status` = 1 ";
     $query = $connection->query($query_str);
     $user = $query->fetch_assoc();
+
     $login = wp_login($user['username'],$user['password'],'wp_signon()');
+
     if($login == 1){
         $query_str = "INSERT INTO `tbl-massage`(`chat_id`, `body`,`title`,`status`,`functions`,`login`,`date`) VALUES ($chatId,'0','0','0','-finish_login-','1',$now)";
         $query = $connection->query($query_str);
         $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=inter your title";
         file_get_contents($url);
         die();
-        }
+    }
     }
     if($massage == '-title-'){
         global $update_id;
@@ -157,6 +201,7 @@ if($massage == '-title-' || $massage == '-body-' || $massage == '-finish-' || $m
             file_get_contents($url); 
             die();                   
         }
+
     }
     if($massage == '-body-'){
         global $update_id;
@@ -171,23 +216,29 @@ if($massage == '-title-' || $massage == '-body-' || $massage == '-finish-' || $m
             $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=body:";
             file_get_contents($url);
             die();
+
         }
     }
     if($massage == '-finish-'){
         $query_str = "SELECT  `body`,`title` FROM `tbl-massage` where chat_id = $chatId and status = 0";
+
         $query = $connection->query($query_str);
         while($posts = $query->fetch_assoc()){
 
             $post[] = $posts;
         }
         if(!empty($post)){
+
             insert( $post[0]['title'], $post[0]['body']);
             $query_str = "UPDATE `tbl-massage` SET `status`= 1 where chat_id = $chatId ";
             $query = $connection->query($query_str);
             $url = "https://api.telegram.org/bot734973442:AAG-xo0OWhcPhrJGNX1fBrNvmq2D4G0KVN8/sendmessage?chat_id=".$chatId."&text=Your post is ready now";
             file_get_contents($url);  
             die();
-            }
+
         }
+        
     }
+}
+
 }
